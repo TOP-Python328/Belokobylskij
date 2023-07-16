407ff83ae0a9c420959760d87b853571d3da7066
BeloV1k <belokobylskij1991@gmail.com>
2023-07-17 00:13:15 +0300

    исправил закомментированный stdout

--- 2023.07.07/5.py
+++ 2023.07.07/5.py
@@ -4,4 +4,6 @@ miles = num_1 + '.' + num_2
 kilometers = round(float(miles) * 1.61, 1)
 print(f'{miles} миль = {kilometers} километров.')
 
+# Введите целую часть: 3
+# Введите дробную часть:48
 # 3.48 миль = 5.6 километров.
