3a2a0ef37f4e790b65f41b88408578a8191b83fc
Gennadiy <renardes@gmail.com>
2023-08-22 20:01:30 +0500

    2023.07.21: комментарии, правки, итоги

--- 2023.07.21/6.py
+++ 2023.07.21/6.py
@@ -1,13 +1,19 @@
 ticket_num = input('Введите свой билет: ')
-lucky_ticket  = ''
-list_digit =[int(digit) for digit in ticket_num]
+lucky_ticket = ''
+list_digit = [int(digit) for digit in ticket_num]
 if sum(list_digit[:3]) == sum(list_digit[3:]):
     lucky_ticket = 'Да'
-else: lucky_ticket = 'Нет'    
+# ИСПОЛЬЗОВАТЬ везде: PEP 8 не рекомендует записывать тело блока в одну строчку с заголовком блока
+else:
+    lucky_ticket = 'Нет'
 print(lucky_ticket)
+
 
 # Введите свой билет: 123321
 # Да
 
 # Введите свой билет: 123456
 # Нет
+
+
+# ИТОГ: отлично — 4/4
